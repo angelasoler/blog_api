46e662839c62e79b7390b034de4906cc76ca9145
angelasoler <manzanaembicicleta@gmail.com>
2022-01-15 22:18:57 -0300

    create and update a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
+
+  rescue_from Exception do |e|
+    render json: { error: e.message }, status: :internal_error
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { error: e.message }, status: 422
+  end
+
   def index
     @posts = Post.published
     render json: @posts, status: 200
@@ -7,5 +16,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     render json: @post, status: 200
+  end
+
+  def create
+    @post = Post.create!(create_params)
+    render json: @post, status: 201
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update!(update_params)
+    render json: @post, status: 200
+  end
+
+  def create_params
+    params.require(:post).permit(:title, :content, :published, :user_id)
+  end
+
+  def update_params
+    params.require(:post).permit(:title, :content, :published)
   end
 end
