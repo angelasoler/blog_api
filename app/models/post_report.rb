e3633fd2d19dff872a6204c04bff62c08f1bb19a
angelasoler <manzanaembicicleta@gmail.com>
2022-01-18 09:47:22 -0300

    generating post reports with active job in asynchrous brack end enqueu with threads pool

---
+++ app/models/post_report.rb
@@ -0,0 +1,18 @@
+class PostReport < Struct.new(:word_count, :word_histogram)
+  def self.generate(post)
+    PostReport.new(
+      Post.content.split.map{ |word| word.gsub(/\W/, '') }.count
+    )
+    calculate_histogram(post)
+  end
+
+  def self.calculate_histogram(post)
+    (post
+      .content
+      .split
+      .map{ |word| word.gsub(/\W/, '') }
+      .map(&:downcase)
+      .group_by{ |word| word }
+      .transform_values(&:size)
+    )
+end
