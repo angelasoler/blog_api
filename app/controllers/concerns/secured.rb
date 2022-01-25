a73ba5177142b0c51eac613b92c563c397bbcd6c
angelasoler <manzanaembicicleta@gmail.com>
2022-01-25 00:56:09 -0300

    wip internal error in json_web_token

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -1,13 +1,28 @@
 module Secured
   def authenticate_user!
-    token_regex = /Bearer (\w+)/
-    headers = request.headers
-    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
-      token = headers['Authorization'].match(token_regex)[1]
-      if(Current.user = User.find_by_auth_token(token))
-        return
+    if(Current.user = user_from_token)
+      return
+    end
+    render json: { error: 'Unauthorized' }, status: 401
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { error: 'Unauthorized' }, status: 401
+  end
+
+  def user_from_token
+    token = get_token_from_auth_header
+    payload = JsonWebToken.verify(token).first.with_indifferent_access
+    if payload.present?
+      User.find_or_create_by(email: payload[:email]) do |user|
+        user.name = payload[:name]
       end
     end
-    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
+
+  def get_token_from_auth_header
+    token_regex = /Bearer (.+)/
+    headers = request.headers
+    if headers['Authorization'].present? && headers['Authorization'].match(token_regex)
+      headers['Authorization'].match(token_regex)[1]
+    end
   end
 end
